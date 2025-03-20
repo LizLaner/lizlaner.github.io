be6a34ac0c5fbcacfdfad91770149589743a63cc
LizLaner <162048324+LizLaner@users.noreply.github.com>
2025-03-20 14:30:31 -0400

    Freewheeling
    testing things out

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <li><router-link to="/">Home</router-link></li>
+                <li><router-link to="/about">About</router-link></li>
+                <li><router-link to="/contact">Contact</router-link></li>
+            </ul>
+        </nav>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+    <h1>TEST test TEST test</h1>
+</div>
+
+
+
+</template>
